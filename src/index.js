1d67ecad0a4df770ff203e7856180a5a533ac53d
Stiv <pablo1012@outlook.com>
2020-08-28 21:28:45 -0300

    Refactor command handling

--- src/index.js
+++ src/index.js
@@ -12,25 +12,61 @@ const client = axios.create({
   baseURL: process.env.API_URL,
 });
 
+const Command = {
+  AddQuestion: 1,
+  AddAnswer: 2,
+  Register: 3,
+};
+
+const commands = {
+  [Command.AddQuestion]: {
+    name: Command.AddQuestion,
+    handler: addQuestion,
+    condition: (msg) => msg.content.startsWith("q:"),
+    message: `
+      In order to ask a question, just start your message with 'q:' and ask your question as you normally would. 
+      If your question is correctly saved a reaction with ✍️ will appear..
+    `,
+  },
+  [Command.AddAnswer]: {
+    name: Command.AddAnswer,
+    handler: addAnswer,
+    condition: (msg) => msg.content.startsWith("> q:"),
+    message: `In order to add an aswer to a question you must use the quote functionality for discord, 
+    If your answer is correctly saved a reaction with ✍️ will appear.`,
+  },
+  [Command.Register]: {
+    name: Command.Register,
+    handler: registerBot,
+    condition: (msg) => msg.content.startsWith("dd!"),
+    message: `To register the bot you must add a BotOwner role on the channel. A user with the BotOwner role 
+    has to call 'dd!' to setup the bot. Follow the instructions that appear next, this will allow you to set a
+    technology for the discussions made in this server.
+    `,
+  },
+};
+
+function shouldHandle(msg) {
+  return !msg.author.bot || msg.channel.guild;
+}
+
 bot.on("message", async (msg) => {
-  if (msg.author.bot) return;
+  if (!shouldHandle) return;
 
-  if (msg.content.startsWith("D!")) {
-    await setUpBot(msg);
-  }
-
-  try {
-    if (msg.content.startsWith("q:")) {
-      await addQuestion(msg);
-      msg.react("✍️");
-    } else if (msg.content.startsWith("> q:")) {
-      await addAnswer(msg);
-      msg.react("✍️");
+  for (let commandKey in commands) {
+    const command = commands[commandKey];
+    if (command.condition(msg)) {
+      console.log(`entered condition ${command.name}`);
+      try {
+        command.handler(msg);
+        msg.react("✍️");
+      } catch (e) {
+        console.error(`Something exploded while handling ${command.name}`);
+        console.error(e);
+      }
+      break;
     }
-  } catch {
-    console.log("something broke");
   }
-
 });
 
 bot.on("guildCreate", (guild) => {
@@ -51,25 +87,19 @@ bot.on("guildCreate", (guild) => {
   //Your other stuff like adding to guildArray
 });
 
-
-async function setupBot() {
-  
-}
+async function registerBot() {}
 async function addQuestion(msg) {
   const { parentID: server } = msg.channel;
   const { id: author } = msg.author;
   const { id: discordMsgId } = msg;
   const text = msg.content.match(qnaparser)[1];
-  await client
-    .post(`question`, {
-      author,
-      text,
-      server,
-      discordMsgId,
-      tech: 1,
-    })
-    .then(console.log);
-  // .catch(console.log);
+  await client.post(`question`, {
+    author,
+    text,
+    server,
+    discordMsgId,
+    tech: 1,
+  });
 }
 
 async function addAnswer(msg) {
@@ -84,16 +114,11 @@ async function addAnswer(msg) {
     text: qtext,
   });
 
-  try {
-    const { data: answerData } = await client.post("answer", {
-      author,
-      text,
-      server,
-      tech: question.qtech,
-      questionId: question.qid,
-    });
-    console.log(answerData);
-  } catch (e) {
-    console.log(e);
-  }
+  const { data: answerData } = await client.post("answer", {
+    author,
+    text,
+    server,
+    tech: question.qtech,
+    questionId: question.qid,
+  });
 }
